b792ce95c34d32dc3cdf898ed69870e16adacb75
zia <ziabadar4991@gmail.com>
2019-06-07 13:38:09 +0200

    order statistic

---
+++ algorithms/order_statistic.h
@@ -0,0 +1,20 @@
+#include <time.h>
+
+// kth smallest element in linear time
+template<typename data_type>
+data_type get_kth_smallest(data_type* array, int l, int r, int k, void (*swap)(data_type&, data_type&))
+{
+    int ran = rand() % (r-l+1) + l;
+    swap(array[r], array[ran]);
+    int j=l;
+    for(int i=l; i<r; i++)
+        if(array[i] < array[r])
+            swap(array[i], array[j++]);
+    swap(array[r], array[j]);
+
+    if(j == k)
+        return array[j];
+    else if(j < k)
+        return get_kth_smallest(array, j+1, r, k, swap);
+    return get_kth_smallest(array, l, j-1, k, swap);
+}
---
+++ tests/order_statistic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <time.h>
+#include "../algorithms/sorting.h"
+#include "../algorithms/order_statistic.h"
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+
+    int size = 100;
+    int sorted[size], unsorted[size];
+
+    for(int i=0; i<size; i++)
+        sorted[i] = unsorted[i] = i;
+
+    unsort(unsorted, size, swap);
+
+    for(int i=0; i<size; i++)
+        cout << sorted[i] << " " << get_kth_smallest(unsorted, 0, size-1, i, swap) << endl;
+}
